9ebdaaf17370e4872df08d71dc335d8e48074136
hoshi <hamh1121@icloud.com>
2025-05-11 18:13:28 +0900

    📝 7주차 실습

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,4 +1,5 @@
 import { responseFromUser } from "../dtos/user.dto.js";
+import { DuplicateUserEmailError } from "../error.js";
 import {
   addUser,
   getUser,
@@ -17,7 +18,7 @@ export const userSignUp = async (data) => {
   });
 
   if (joinUserId === null) {
-    throw new Error("이미 존재하는 이메일입니다.");
+    throw new DuplicateUserEmailError("이미 존재하는 이메일입니다.",data);
   }
 
   for (const preference of data.preferences) {
