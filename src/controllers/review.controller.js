98e3b3855719133f04dd2d2f4bd659483369d897
hoshi <hamh1121@icloud.com>
2025-05-20 13:25:48 +0900

    📝 8주차 실습

--- src/controllers/review.controller.js
+++ src/controllers/review.controller.js
@@ -18,6 +18,41 @@ export const handleUploadReview = async (req, res, next) => {
 };
 
 export const handleListStoreReviews = async (req, res, next) => {
+  /*
+  #swagger.summary = '상점 리뷰 목록 조회 API';
+  #swagger.responses[200] = {
+    description: "상점 리뷰 목록 조회 성공 응답",
+    content: {
+      "application/json": {
+        schema: {
+          type: "object",
+          properties: {
+            resultType: { type: "string", example: "SUCCESS" },
+            error: { type: "object", nullable: true, example: null },
+            success: {
+              type: "object",
+              properties: {
+                data: {
+                  type: "array",
+                  items: {
+                    type: "object",
+                    properties: {
+                      id: { type: "number" },
+                      store: { type: "object", properties: { id: { type: "number" }, name: { type: "string" } } },
+                      user: { type: "object", properties: { id: { type: "number" }, email: { type: "string" }, name: { type: "string" } } },
+                      content: { type: "string" }
+                    }
+                  }
+                },
+                pagination: { type: "object", properties: { cursor: { type: "number", nullable: true } }}
+              }
+            }
+          }
+        }
+      }
+    }
+  };
+*/
   console.log("리뷰 로딩 요청이 발생하였습니다!");
   console.log("body: ", req.body);
   const review = await getStoreReviews(bodyToReviewList(req.params, req.query));
